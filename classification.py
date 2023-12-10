27a6276b6654baa11c6a8c9896e007f38c553f30
martynagrygiel <grygielmartyna439@gmail.com>
2023-12-11 00:44:57 +0100

    prompt final version

--- classification.py
+++ classification.py
@@ -97,7 +97,8 @@ topics_a = [
     "rząd pis (prawo i sprawiedliwość)",
     "inflacja",
     "przyjmowanie imigrantów",
-    "społeczność LGBT",
+    "LGBT (społeczność LGBT, queer, homoseksualizm itp.)",
+    "kobiety",
     "węgiel",
     "program 500 plus",
     "program 800 plus",
@@ -107,7 +108,7 @@ topics_a = [
     "WOŚP",
     "jurek owsiak",
     "feminizm",
-    "edukacja seksualna",
+    "edukacja seksualna (wdż)",
     "legalizacja marihuany",
     "telewizja publiczna (tvp)",
     "dotacje dla rolników",
@@ -124,7 +125,7 @@ topics_a = [
     "publiczna opieka zdrowotna (nfz)",
     "smoleńsk",
     "pomoc humanitarna na Ukrainie",
-    "równość płac względem płci",
+    "równość płci",
     "zwiększenie wydatków na wojsko",
     "trybunał konstytucyjny",
     "niezależność sądów (sądownictwo)",
@@ -151,11 +152,11 @@ topics_a = [
 def analyze_text(text: str) -> str:
 
     system_content = """
-     You are helpful assistant with massive knowledge about Polish politics. You spend the whole days for last 40 years reading political tweets and watching Parliament sessions. You've read all tweets that were ever posted on Twitter. This is your only job, just to read about Polish politics. You are not offensive.
+     You are helpful assistant with knowledge about Polish politics and economy. In past few months you have been studying polish Tweets and parliamentary sessions to be able to provide clear, unbiased information about political related topics.
     """
 
     content = """
-    In the text given below find fragments that covers one of provided topics."""  + f"""
+    In the provided text find fragments that cover one of provided topics."""  + f"""
 
     Text: {text}
 
@@ -164,8 +165,8 @@ def analyze_text(text: str) -> str:
 
 
     """+"""
-    Try to use topics from the list, but if you are unable to classify the text into any of the given topics, use your judgment to determine the topic based on context. Do NOT make up topic that doesn't match text. If it's hard to determine topic return empty JSON object.
-    Then determine the sentiment towards that topic as negative, positive and neutral.
+    Try to use topics from the list, but if you are unable to classify the text into any of the given topics, use your judgment to determine the topic based on contex of the text. Do NOT make up topic that doesn't match text. If it's hard to determine topic return JSON objest with empty topic and sentiment fields.
+    Then determine the sentiment towards that topic as negative, positive or neutral.
     Return the results as a list of JSON objects, where each object has fields:
 
 
@@ -174,41 +175,36 @@ def analyze_text(text: str) -> str:
     "sentiment": sentiment}.
 
 
-
-
     Here are three examples that will help you understand the task:
 
-    
     Example 1:
-    Input text: "od lat wiadomo że pis to partia ogromnej imigracji poza wszelką kontrolą"
+    Input: "od lat wiadomo że pis to partia ogromnej imigracji poza wszelką kontrolą"
 
 
-    Output JSON: "[{"text" : „od lat wiadomo że pis to partia ogromnej imigracji poza wszelką kontrolą”,  
+    Output: "[{"text" : „od lat wiadomo że pis to partia ogromnej imigracji poza wszelką kontrolą”,  
                  "topic" : „rząd pis (prawo i sprawiedliwość)”,
                  "sentiment”: negative}, 
                  {"text" : „od lat wiadomo że pis to partia ogromnej imigracji poza wszelką kontrolą”,  
                  "topic" : „przyjmowanie imigrantów”,
                  "sentiment”: negative}]".
 
+
                  
-
-
     Example 2:
-    Input text: „pan minister grzebalczyk z pisu nie zawiódł to nie jest samolot tylko kadłub samolotu jednostka ma być w pełni wyposażona i przygotowana do eksploatacji pod koniec 2026 roku”
+    Input: „pan minister grzebalczyk z pisu nie zawiódł to nie jest samolot tylko kadłub samolotu jednostka ma być w pełni wyposażona i przygotowana do eksploatacji pod koniec 2026 roku”
 
 
-    Output JSON: "{"text" : „pan minister grzebalczyk z pisu nie zawiódł to nie jest samolot tylko kadłub samolotu jednostka ma być w pełni wyposażona i przygotowana do eksploatacji pod koniec 2026 roku”,  
+    Output: "[{"text" : „pan minister grzebalczyk z pisu nie zawiódł to nie jest samolot tylko kadłub samolotu jednostka ma być w pełni wyposażona i przygotowana do eksploatacji pod koniec 2026 roku”,  
                  "topic" : „rząd pis (prawo i sprawiedliwość)”,
-                 "sentiment”: „positive”}".
+                 "sentiment”: „positive”}]".
     
                  
-
-                 
+ 
     Example 3:
-    Input text: „W głowach wam się poprzewracało z tym feminizmem przerywanie ciąży to zdecydowana przesada państwo powinno dofinansowywać in-vitro i skupiać się na tym żeby więcej dzieci się rodziło niż umierało jeszcze nienarodzonych”
+    Input: „W głowach wam się poprzewracało z tym feminizmem przerywanie ciąży to zdecydowana przesada państwo powinno dofinansowywać in-vitro i skupiać się na tym żeby więcej dzieci się rodziło niż umierało jeszcze nienarodzonych”
 
 
-    Output JSON: "[{„text" : „W głowach wam się poprzewracało z tym feminizmem”,  
+    Output: "[{„text" : „W głowach wam się poprzewracało z tym feminizmem”,  
                  "topic" : „feminizm”,
                  "sentiment”: „negatywny”}, 
                  {„text" : „przerywanie ciąży to zdecydowana przesada”,  
@@ -221,66 +217,6 @@ def analyze_text(text: str) -> str:
                  "topic" : „aborcja”,
                  "sentiment”: „negatywny”}]".
     """
-
-    # content = f"""
-    # Poniższa lista zawiera pary klucz - wartość reprezentujące tematy (klucze) i przykładowe słowa kluczowe odnoszące się do danego tematu (wartości), które pozwalają go lepiej zrozumieć.
-
-    #     {topics}
-
-
-    # Znajdź w podanym poniżej tekście fragmenty odpowiadające na jeden z tematów zawartych w liście oraz określ stosunek autora tekstu do wypowiedzi jako  pozytywny, negatywny lub neutralny. Staraj się wykorzystywać te tematy, jednak jeśli nie jesteś w stanie zaklasyfikować tekstu do żadnego z podanych tematów spróbuj wyciągnąć go z tekstu.
-
-
-    #     {text}
-    # """ + """
-    
-    # Zwróć wyniki jako listę obiektów JSON, gdzie każdy obiekt posiada pola:
-
-    #     "{"text" : fragment dostarczonego tekstu,  
-    #     "topic" : temat wypowiedzi,
-    #     "sentiment”: stosunek autora do wypowiedzi}."
-
-
-
-    ## Do NOT make up topic that doesn't match text. If it's hard to determine topic return empty JSON object.
-
-
-
-     
-    # Poniżej zawarto dwa przykłady:
-
-    # Przykład pierwszy:
-    # Tekst wejściowy: 
-    #     „pis jest znany z przecinania wstęgi po kilka razy więc i pan minister gróbarczyk nie zawiódł to nie jest prom tylko kadłub promu jednostka ma być w pełni wyposażona i przygotowana do eksploatacji pod koniec 2024 roku”
-
-
-    # Wyjście:
-    #     {"text" : „pis jest znany z przecinania wstęgi po kilka razy więc i pan minister gróbarczyk nie zawiódł to nie jest prom tylko kadłub promu jednostka ma być w pełni wyposażona i przygotowana do eksploatacji pod koniec 2024 roku”,  
-    #     "topic" : „rządy pis”,
-    #     "sentiment”: „pozytywny”}.
-
-        
-
-    # Przykład drugi:
-    # Tekst wejściowy:
-    # „W głowach wam się poprzewracało z tym feminizmem przerywanie ciąży to zdecydowana przesada państwo powinno dofinansowywać in-vitro i skupiać się na tym żeby więcej dzieci się rodziło niż umierało jeszcze nienarodzonych”
-
-
-    # Wyjście:
-    # [{„text" : „W głowach wam się poprzewracało z tym feminizmem”,  
-    #     "topic" : „feminizm”,
-    #     "sentiment”: „negatywny”}, 
-    # {„text" : „przerywanie ciąży to zdecydowana przesada”,  
-    #     "topic" : „aborcja”,
-    #     "sentiment”: „negatywny”},
-    # {„text" : „państwo powinno dofinansowywać in-vitro”,  
-    #     "topic" : „in-vitro”,
-    #     "sentiment”: „pozytywny”},
-    # {„text" : „i skupiać się na tym żeby więcej dzieci się rodziło niż umierało jeszcze nienarodzonych”,  
-    #     "topic" : „aborcja”,
-    #     "sentiment”: „negatywny”}].
-
-    # """
     
     if len(text) <= 10000:
         model = 'gpt-3.5-turbo'
@@ -293,7 +229,7 @@ def analyze_text(text: str) -> str:
         model=model,
         temperature=0.45,
         messages=[
-            # {"role": "system", "content": system_content},
+            {"role": "system", "content": system_content},
             {"role": "user", "content": content}
 
         ]
